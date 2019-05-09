e948bffc7cba4e86326d7fec6f5714b05d17a4b5
Richard Brandon <Richard.Brandon@gmail.com>
2019-05-09 14:10:55 -0400

    fix empty dirs

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -173,8 +173,8 @@ class RoboFile extends \Robo\Tasks
 
         $tasks[] = $this->taskExec('docker-compose --verbose pull --parallel');
         $tasks[] = $this->taskExec('docker-compose up -d');
-        $tasks[] = $this->taskExec('docker-compose exec -T php chmod -R 777 /home/wodby/.composer');
         $tasks[] = $this->taskExec('docker-compose exec -T php ln -s /home/wodby/docker-cache /home/wodby/.composer/cache');
+        $tasks[] = $this->taskExec('docker-compose exec -T php chmod -R 777 /home/wodby/.composer/cache');
         $tasks[] = $this->taskExec('make');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config/phpunit.xml web/core/phpunit.xml');
         $tasks[] = $this->taskExec('docker-compose exec -T php cp .travis/config//bootstrap.php web/core/tests/bootstrap.php');
